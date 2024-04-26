1af0e5faa241be476f54769c6fb13de7880dc24d
priyanshidev <167754051+priyanshidev@users.noreply.github.com>
2024-04-26 12:22:07 +0530

    Update blog.tsx

--- BlogPost/blog.tsx
+++ BlogPost/blog.tsx
@@ -4,7 +4,7 @@ const blog = () => {
   return (
     <div>
       blogs Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-      expedita, error illo laudantium commodi culpa dignissimos ipsum autem vel
+      expedita, error illo lalkkklllllllludantium commodi culpa dignissimos ipsum autem vel
       pariatur magnam maxime, reprehenderit quis nobis, blanditiis dolorum esse
       sed quasi?<p>iyuuuuuuuuuuu</p>
     </div>
