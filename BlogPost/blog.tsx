b85672307ab760f2b1159bbc09df0bd2b25f6024
priyanshidev <priyanshi.pixeldart@gmail.com>
2024-04-26 09:54:06 +0530

    issue fixed

--- BlogPost/blog.tsx
+++ BlogPost/blog.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const blog = () => {
-  return <div>xfsdfsdf</div>;
+  return (
+    <div>
+      blogs Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+      expedita, error illo laudantium commodi culpa dignissimos ipsum autem vel
+      pariatur magnam maxime, reprehenderit quis nobis, blanditiis dolorum esse
+      sed quasi?
+    </div>
+  );
 };
 
 export default blog;
